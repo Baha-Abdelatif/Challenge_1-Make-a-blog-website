01f065dbd97c438f85e2499b8200c0d531d234d2
Baha-Abdelatif <baha.abdelatif@gmail.com>
2020-03-23 13:20:22 +0100

    Setting up our router and render of homepage

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+  res.render("home");
+});
 
 
 
@@ -27,7 +32,6 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
