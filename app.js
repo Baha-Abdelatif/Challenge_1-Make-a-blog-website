c8d36a8dbcbb62296474e30ecd3bb669a3ef9027
Baha-Abdelatif <baha.abdelatif@gmail.com>
2020-03-23 17:24:06 +0100

    Use ExpressJs to set dynamics urls for our posts

--- app.js
+++ app.js
@@ -71,6 +71,15 @@ app.post('/compose', (req, res) => {
   res.redirect('/');
 })
 
+app.get('/posts/:postName', (req, res) => {
+  console.log(req.params.postName);
+  res.render("home", {
+    homeStartingContent,
+    pageTitle: 'Home',
+    posts
+  });
+})
+
 
 
 
